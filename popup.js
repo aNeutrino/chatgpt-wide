e6cbb3a0bb2c74e454f7f5af2c788d336fc3f309
aNeutrino <piotr.modrzyk@gmail.com>
2024-05-08 12:05:55 +0200

    From now is click based not hostname based

--- popup.js
+++ popup.js
@@ -1,16 +1,28 @@
 document.getElementById('toggleStyle').addEventListener('click', function() {
-    chrome.tabs.executeScript({
-        code: `
-            document.querySelectorAll('*').forEach(function(node) {
-                if (node.style.maxWidth) {
-                    node.style.maxWidth = 'none';
-                }
-            });
-
-            var style = document.createElement('style');
-            style.innerHTML = '* { max-width: none !important; }';
-            document.head.appendChild(style);
-        `
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var currentTab = tabs[0];
+        chrome.scripting.executeScript({
+            target: {tabId: currentTab.id},
+            function: toggleMaxWidth
+        });
     });
 });
 
+function toggleMaxWidth() {
+    const key = 'styleToggled';
+    const styleId = 'max-width-toggle-style';
+
+    chrome.storage.local.get([key], function(result) {
+        let styleExists = document.getElementById(styleId);
+        if (result[key] && styleExists) {
+            styleExists.remove();
+            chrome.storage.local.set({[key]: false});
+        } else {
+            let style = document.createElement('style');
+            style.id = styleId;
+            style.innerHTML = '* { max-width: none !important; }';
+            document.head.appendChild(style);
+            chrome.storage.local.set({[key]: true});
+        }
+    });
+}
